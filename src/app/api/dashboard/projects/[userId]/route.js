16eb39a75be42bbce35dd0c766c00d1f41215523
Victordi10 <cordobav444@gmail.com>
2025-03-22 22:03:35 -0500

    aver

--- src/app/api/dashboard/projects/[userId]/route.js
+++ src/app/api/dashboard/projects/[userId]/route.js
@@ -23,7 +23,7 @@ export async function POST(req) {
                 }
             },
             include: {
-                members: true // 🔹 Cargar los miembros en la respuesta
+                members: true 
             }
         });
         return successResponse("Proyecto creado exitosamente", newProject);
